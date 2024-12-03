7519211f7958f196d6e7a547ab7a0404b362c2ce
Matias Israelson <57065102+israpps@users.noreply.github.com>
2024-12-03 12:27:26 -0300

    simplify `binaries.h`

--- binaries.h
+++ binaries.h
@@ -16,239 +16,86 @@
 
 #include <stdint.h>
 
-extern uint8_t iomanX;
-extern u32 size_iomanX;
-
-extern uint8_t fileXio;
-extern u32 size_fileXio;
-
-extern uint8_t freesio2;
-extern u32 size_freesio2;
-
-extern uint8_t freepad;
-extern u32 size_freepad;
-
-extern uint8_t mcman;
-extern u32 size_mcman;
-
-extern uint8_t mcserv;
-extern u32 size_mcserv;
-
-extern uint8_t USBD;
-extern u32 size_USBD;
-
-extern uint8_t USBHDFSD;
-extern u32 size_USBHDFSD;
-
-extern uint8_t MECHAPROXY_irx;
-extern u32 size_MECHAPROXY_irx;
-
-extern uint8_t IndieFlower;
-extern u32 size_IndieFlower;
-
-extern uint8_t MASSWATCHER_irx;
-extern u32 size_MASSWATCHER_irx;
-
-extern uint8_t pwr50k;
-extern u32 size_pwr50k;
-
-extern uint8_t pwr70k;
-extern u32 size_pwr70k;
-
-extern uint8_t pwr90k;
-extern u32 size_pwr90k;
-
-extern uint8_t pwrpsx1;
-extern u32 size_pwrpsx1;
-
-extern uint8_t pwrpsx2;
-extern u32 size_pwrpsx2;
-
-extern uint8_t pwrtvcombo;
-extern u32 size_pwrtvcombo;
-
-extern uint8_t frame_001;
-extern u32 size_frame_001;
-
-extern uint8_t frame_002;
-extern u32 size_frame_002;
-
-extern uint8_t frame_003;
-extern u32 size_frame_003;
-
-extern uint8_t frame_004;
-extern u32 size_frame_004;
-
-extern uint8_t frame_005;
-extern u32 size_frame_005;
-
-extern uint8_t frame_006;
-extern u32 size_frame_006;
-
-extern uint8_t frame_007;
-extern u32 size_frame_007;
-
-extern uint8_t frame_008;
-extern u32 size_frame_008;
-
-extern uint8_t frame_009;
-extern u32 size_frame_009;
-
-extern uint8_t frame_010;
-extern u32 size_frame_010;
-
-extern uint8_t frame_011;
-extern u32 size_frame_011;
-
-extern uint8_t frame_012;
-extern u32 size_frame_012;
-
-extern uint8_t frame_013;
-extern u32 size_frame_013;
-
-extern uint8_t frame_014;
-extern u32 size_frame_014;
-
-extern uint8_t frame_015;
-extern u32 size_frame_015;
-
-extern uint8_t frame_016;
-extern u32 size_frame_016;
-
-extern uint8_t frame_017;
-extern u32 size_frame_017;
-
-extern uint8_t frame_018;
-extern u32 size_frame_018;
-
-extern uint8_t frame_019;
-extern u32 size_frame_019;
-
-extern uint8_t frame_020;
-extern u32 size_frame_020;
-
-extern uint8_t frame_021;
-extern u32 size_frame_021;
-
-extern uint8_t frame_022;
-extern u32 size_frame_022;
-
-extern uint8_t frame_023;
-extern u32 size_frame_023;
-
-extern uint8_t frame_024;
-extern u32 size_frame_024;
-
-extern uint8_t frame_025;
-extern u32 size_frame_025;
-
-extern uint8_t frame_026;
-extern u32 size_frame_026;
-
-extern uint8_t frame_027;
-extern u32 size_frame_027;
-
-extern uint8_t frame_028;
-extern u32 size_frame_028;
-
-extern uint8_t frame_029;
-extern u32 size_frame_029;
-
-extern uint8_t frame_030;
-extern u32 size_frame_030;
-
-extern uint8_t frame_031;
-extern u32 size_frame_031;
-
-extern uint8_t frame_032;
-extern u32 size_frame_032;
-
-extern uint8_t frame_033;
-extern u32 size_frame_033;
-
-extern uint8_t frame_034;
-extern u32 size_frame_034;
-
-extern uint8_t frame_035;
-extern u32 size_frame_035;
-
-extern uint8_t frame_036;
-extern u32 size_frame_036;
-
-extern uint8_t frame_037;
-extern u32 size_frame_037;
-
-extern uint8_t frame_038;
-extern u32 size_frame_038;
-
-extern uint8_t frame_039;
-extern u32 size_frame_039;
-
-extern uint8_t frame_040;
-extern u32 size_frame_040;
-
-extern uint8_t frame_041;
-extern u32 size_frame_041;
-
-extern uint8_t frame_042;
-extern u32 size_frame_042;
-
-extern uint8_t frame_043;
-extern u32 size_frame_043;
-
-extern uint8_t frame_044;
-extern u32 size_frame_044;
-
-extern uint8_t frame_045;
-extern u32 size_frame_045;
-
-extern uint8_t frame_046;
-extern u32 size_frame_046;
-
-extern uint8_t frame_047;
-extern u32 size_frame_047;
-
-extern uint8_t frame_048;
-extern u32 size_frame_048;
-
-extern uint8_t frame_049;
-extern u32 size_frame_049;
-
-extern uint8_t frame_050;
-extern u32 size_frame_050;
-
-extern uint8_t frame_051;
-extern u32 size_frame_051;
-
-extern uint8_t frame_052;
-extern u32 size_frame_052;
-
-extern uint8_t frame_053;
-extern u32 size_frame_053;
-
-extern uint8_t frame_054;
-extern u32 size_frame_054;
-
-extern uint8_t frame_055;
-extern u32 size_frame_055;
-
-extern uint8_t frame_056;
-extern u32 size_frame_056;
-
-extern uint8_t frame_057;
-extern u32 size_frame_057;
-
-extern uint8_t frame_058;
-extern u32 size_frame_058;
-
-extern uint8_t frame_059;
-extern u32 size_frame_059;
-
-extern uint8_t frame_060;
-extern u32 size_frame_060;
-
-extern uint8_t frame_061;
-extern u32 size_frame_061;
-
-extern uint8_t frame_062;
-extern u32 size_frame_062;
+#define BIN2C_EXTERN(x) \
+    extern uint8_t x; \
+    extern u32 size_##x
+
+BIN2C_EXTERN(iomanX);
+BIN2C_EXTERN(fileXio);
+BIN2C_EXTERN(freesio2);
+BIN2C_EXTERN(freepad);
+BIN2C_EXTERN(mcman);
+BIN2C_EXTERN(mcserv);
+BIN2C_EXTERN(USBD);
+BIN2C_EXTERN(USBHDFSD);
+BIN2C_EXTERN(MECHAPROXY_irx);
+BIN2C_EXTERN(IndieFlower);
+BIN2C_EXTERN(MASSWATCHER_irx);
+BIN2C_EXTERN(pwr50k);
+BIN2C_EXTERN(pwr70k);
+BIN2C_EXTERN(pwr90k);
+BIN2C_EXTERN(pwrpsx1);
+BIN2C_EXTERN(pwrpsx2);
+BIN2C_EXTERN(pwrtvcombo);
+BIN2C_EXTERN(frame_001);
+BIN2C_EXTERN(frame_002);
+BIN2C_EXTERN(frame_003);
+BIN2C_EXTERN(frame_004);
+BIN2C_EXTERN(frame_005);
+BIN2C_EXTERN(frame_006);
+BIN2C_EXTERN(frame_007);
+BIN2C_EXTERN(frame_008);
+BIN2C_EXTERN(frame_009);
+BIN2C_EXTERN(frame_010);
+BIN2C_EXTERN(frame_011);
+BIN2C_EXTERN(frame_012);
+BIN2C_EXTERN(frame_013);
+BIN2C_EXTERN(frame_014);
+BIN2C_EXTERN(frame_015);
+BIN2C_EXTERN(frame_016);
+BIN2C_EXTERN(frame_017);
+BIN2C_EXTERN(frame_018);
+BIN2C_EXTERN(frame_019);
+BIN2C_EXTERN(frame_020);
+BIN2C_EXTERN(frame_021);
+BIN2C_EXTERN(frame_022);
+BIN2C_EXTERN(frame_023);
+BIN2C_EXTERN(frame_024);
+BIN2C_EXTERN(frame_025);
+BIN2C_EXTERN(frame_026);
+BIN2C_EXTERN(frame_027);
+BIN2C_EXTERN(frame_028);
+BIN2C_EXTERN(frame_029);
+BIN2C_EXTERN(frame_030);
+BIN2C_EXTERN(frame_031);
+BIN2C_EXTERN(frame_032);
+BIN2C_EXTERN(frame_033);
+BIN2C_EXTERN(frame_034);
+BIN2C_EXTERN(frame_035);
+BIN2C_EXTERN(frame_036);
+BIN2C_EXTERN(frame_037);
+BIN2C_EXTERN(frame_038);
+BIN2C_EXTERN(frame_039);
+BIN2C_EXTERN(frame_040);
+BIN2C_EXTERN(frame_041);
+BIN2C_EXTERN(frame_042);
+BIN2C_EXTERN(frame_043);
+BIN2C_EXTERN(frame_044);
+BIN2C_EXTERN(frame_045);
+BIN2C_EXTERN(frame_046);
+BIN2C_EXTERN(frame_047);
+BIN2C_EXTERN(frame_048);
+BIN2C_EXTERN(frame_049);
+BIN2C_EXTERN(frame_050);
+BIN2C_EXTERN(frame_051);
+BIN2C_EXTERN(frame_052);
+BIN2C_EXTERN(frame_053);
+BIN2C_EXTERN(frame_054);
+BIN2C_EXTERN(frame_055);
+BIN2C_EXTERN(frame_056);
+BIN2C_EXTERN(frame_057);
+BIN2C_EXTERN(frame_058);
+BIN2C_EXTERN(frame_059);
+BIN2C_EXTERN(frame_060);
+BIN2C_EXTERN(frame_061);
+BIN2C_EXTERN(frame_062);
